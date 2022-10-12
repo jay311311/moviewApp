8d9583cff6472709ef8a61aa17bf1238fd91301f
[파인원]김주은 <jay311311@pineone.com>
2022-10-12 19:05:16 +0900

    create UITabbarController and few viewControllers

---
+++ moviewApp/HomeViewController.swift
@@ -0,0 +1,38 @@
+//
+//  HomeViewController.swift
+//  moviewApp
+//
+//  Created by Jooeun Kim on 2022/10/12.
+//
+
+import UIKit
+import SnapKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+
+        setupUI()
+    }
+    
+    let mainTitle:UILabel  = {
+        let label  =  UILabel()
+        label.text =  "홈"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    
+    func setupUI(){
+        self.view.addSubview(mainTitle)
+        
+        mainTitle.snp.makeConstraints{
+            $0.center.equalTo(self.view)
+        }
+    }
+
+  
+
+}
--- moviewApp/ViewController.swift
+++ moviewApp/ViewController.swift
@@ -12,25 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .white
-        setupUI()
         
     }
 
-    let mainTitle:UILabel  = {
-        let label  =  UILabel()
-        label.text =  "메인뷰"
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
 
-    
-    func setupUI(){
-        self.view.addSubview(mainTitle)
-        
-        mainTitle.snp.makeConstraints{
-            $0.center.equalTo(self.view)
-        }
-    }
 }
 
